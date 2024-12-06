1ce24b4be2bccef7e0fbd9fcd69e93595c4264a8
haiztvn <haiztvn@gmail.com>
2024-12-06 10:58:03 +0700

    commit 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
